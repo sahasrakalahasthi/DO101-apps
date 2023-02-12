6663e88ac60061c2cf9e72ca84d814d65e77ab84
sahasrakalahasthi <kalahasthisahasra@gmail.com>
2023-02-12 15:19:22 +0530

    commit changes

--- weather/app.js
+++ weather/app.js
@@ -12,6 +12,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use('/', indexRouter);
+var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
